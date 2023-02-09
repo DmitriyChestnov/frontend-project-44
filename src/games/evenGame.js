b1dfb25fdb06351144cf0f399e1c78fe144b3b5c
Dmitriy Chestnov <invheor@gmail.com>
2023-02-09 19:26:38 +0300

    fix brain-even. and evenFame

--- src/games/evenGame.js
+++ src/games/evenGame.js
@@ -1,25 +1,23 @@
-import * as readlineSync from "readline-sync";
+import * as readlineSync from 'readline-sync';
 
-const generateNumber = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1)) + min;
-
+const generateNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 const evenGame = () => {
-  console.log("Welcome to the Brain Games!");
-  const name = readlineSync.question("May I have your name?");
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name?');
   console.log(`Hello, ${name}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no"');
 
-  for (let i = 0; i < 3; ) {
+  for (let i = 0; i < 3;) {
     const number = generateNumber(1, 100);
-    const answerTrue = number % 2 === 0 ? "yes" : "no";
+    const answerTrue = number % 2 === 0 ? 'yes' : 'no';
     console.log(`Quetion: ${number}!`);
-    const answerPlayer = readlineSync.question("Your answer:");
+    const answerPlayer = readlineSync.question('Your answer:');
     if (answerPlayer === answerTrue) {
-      console.log("Correct!");
+      console.log('Correct!');
       i += 1;
     } else {
       console.log(
-        `'${answerPlayer}' is wrong answer ;(. Correct answer was '${answerTrue}'.`
+        `'${answerPlayer}' is wrong answer ;(. Correct answer was '${answerTrue}'.`,
       );
       console.log(`Let's try again, ${name}!`);
       return false;
