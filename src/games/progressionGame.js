1ccd4afd9f740bbbfc555d214b6f10aab95e9d41
Dmitriy Chestnov <invheor@gmail.com>
2023-02-20 19:53:48 +0300

    fix progressionGame.js

--- src/games/progressionGame.js
+++ src/games/progressionGame.js
@@ -2,26 +2,30 @@ import getRandomNumber from '../getRandomNumber.js';
 import generalGameLogic from '../index.js';
 
 const gameRules = 'What number is missing in the progression?';
+const progressionLength = 10;
 
-const gameRound = () => {
-  const progressionLength = 10;
-
-  let num = getRandomNumber(1, 19);
-  const step = getRandomNumber(2, 19);
-
-  const array = [num];
+const getProgression = (numStart, step) => {
+  const result = [numStart];
+  let num = numStart;
 
   for (let i = 0; i < progressionLength; i += 1) {
     num += step;
-    array.push(num);
+    result.push(num);
   }
+  return result;
+};
 
-  const arrayIndex = getRandomNumber(0, 9);
-  let answerTrue = array[arrayIndex];
-  array[arrayIndex] = '..';
+const gameRound = () => {
+  const num = getRandomNumber(1, 19);
+  const step = getRandomNumber(2, 19);
+  const progression = getProgression(num, step);
+
+  const progIndex = getRandomNumber(0, 9);
+  let answerTrue = progression[progIndex];
+  progression[progIndex] = '..';
 
   answerTrue = String(answerTrue);
-  const question = array.join(' ');
+  const question = progression.join(' ');
   return [question, answerTrue];
 };
 
