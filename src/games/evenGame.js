deb37b73156a250453032dd3c58c9c51ea432ca2
Dmitriy Chestnov <invheor@gmail.com>
2023-02-20 19:14:32 +0300

    fix evenGame.js

--- src/games/evenGame.js
+++ src/games/evenGame.js
@@ -3,9 +3,11 @@ import generalGameLogic from '../index.js';
 
 const gameRules = 'Answer "yes" if the number is even, otherwise answer "no"';
 
+const isEven = (num) => (num % 2 === 0);
+
 const gameRound = () => {
   const question = getRandomNumber(1, 100);
-  const answerTrue = question % 2 === 0 ? 'yes' : 'no';
+  const answerTrue = isEven(question) ? 'yes' : 'no';
 
   return [question, answerTrue];
 };
