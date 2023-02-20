2620999ab47e81f8a4e33f3ec0002b67ee2d8e7e
Dmitriy Chestnov <invheor@gmail.com>
2023-02-20 19:06:20 +0300

    fix calcGame.js

--- src/games/calcGame.js
+++ src/games/calcGame.js
@@ -2,34 +2,32 @@ import getRandomNumber from '../getRandomNumber.js';
 import generalGameLogic from '../index.js';
 
 const gameRules = 'What is the result of the expression?';
+const symbols = ['+', '-', '*'];
 
-const gameRound = () => {
-  const symbols = ['+', '-', '*'];
+const calculate = (a, b, operation) => {
+  switch (operation) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    default:
+      return null;
+  }
+};
+
+const getAnswerAndQuestion = () => {
   const operation = symbols[getRandomNumber(0, 2)];
 
   const firstOperand = getRandomNumber(1, 30);
   const secondOperand = getRandomNumber(1, 30);
 
   const question = `${firstOperand} ${operation} ${secondOperand}`;
-  let answerTrue = '';
-
-  switch (operation) {
-    case '+':
-      answerTrue = firstOperand + secondOperand;
-      break;
-    case '-':
-      answerTrue = firstOperand - secondOperand;
-      break;
-    case '*':
-      answerTrue = firstOperand * secondOperand;
-      break;
-    default:
-      return null;
-  }
-  answerTrue = String(answerTrue);
+  const answerTrue = calculate(firstOperand, secondOperand, operation).toString();
   return [question, answerTrue];
 };
 
-const calcGame = () => generalGameLogic(gameRules, gameRound);
+const calcGame = () => generalGameLogic(gameRules, getAnswerAndQuestion);
 
 export default calcGame;
